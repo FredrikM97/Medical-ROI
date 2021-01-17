d115649a0bc8c7fb3cd5b5e0c7760b0571edd0b7
Fredrik <Fredrik9779@gmail.com>
2021-01-17 20:26:35 +0100

    Separated data loading objects to their own file

---
+++ Development/dataloader.py
@@ -0,0 +1,37 @@
+import os
+import xml.etree.ElementTree as ET
+from functools import reduce
+
+def get_images(rootdir=None)-> dict:
+    """Get all files from folder and subfolder of nii images. Based on ADNI directory paths!"""
+    columns=[
+        'projectIdentifier',
+        'subject.subjectIdentifier',
+        'ImageProtocol.description',
+        'dateAcquired',
+        'subject.study.imagingProtocol.imageUID', 
+        'filename',
+        'path'
+    ]
+    
+    assert rootdir, "No rootdir selected"
+    contents = []
+    temp_dir = {}
+    start = rootdir.rfind(os.sep) + 1
+    for path, dirs, files in os.walk(rootdir):
+        folders = path[start:].split(os.sep)
+        subdir = dict.fromkeys(files)
+        contents.extend((*folders,file, path + "/"+file) for file in files)
+
+        parent = reduce(dict.get, folders[:-1], temp_dir)
+        parent[folders[-1]] = subdir
+
+    return temp_dir
+
+def get_XML(path=None) -> iter:
+    assert path, "No path defined"
+    for filename in os.listdir(path):
+        if not filename.endswith('.xml'): continue
+        fullname = os.path.join(path, filename)
+        tree = ET.parse(fullname)
+        yield tree
