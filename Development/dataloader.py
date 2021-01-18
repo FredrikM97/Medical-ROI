ca041ec9a3356bccb4ac70e8bdee88ee0394ae2a
Fredrik <Fredrik9779@gmail.com>
2021-01-18 14:39:07 +0100

    Corrected import, assert for copy_file, implemented load_nii_data and save_to_categorised_images

--- Development/dataloader.py
+++ Development/dataloader.py
@@ -2,6 +2,7 @@ import os
 import xml.etree.ElementTree as ET
 from functools import reduce
 import shutil
+import nibabel as nib
 
 def get_images(rootdir=None)-> dict:
     """Get all files from folder and subfolder of nii images. Based on ADNI directory paths!"""
@@ -30,6 +31,7 @@ def get_images(rootdir=None)-> dict:
     return contents, columns
 
 def get_XML(path=None) -> iter:
+    "Load XML from dictory and return a generator"
     assert path, "No path defined"
     for filename in os.listdir(path):
         if not filename.endswith('.xml'): continue
@@ -39,7 +41,19 @@ def get_XML(path=None) -> iter:
         
 def copy_file(src, dest):
     "Copy file from source dir to dest dir. Note that the path must exist where folders should be placed!"
+    assert os.path.exists(src), "Source file does not exists"
+    
     if not os.path.exists(dest):
         shutil.copy(src, dest)
         return 'ok' 
     return "fail"
+
+def save_to_categorised_images(output_df, base_dir)->None:
+    "Save images to categories based on parameters from dataframe"
+    output_df.apply(lambda row: copy_file(str(row['path']), f"{base_dir}/{row['subject.researchGroup']}/{row['filename']}"), axis=1)
+    
+def load_nii_data(path) -> iter:
+    files = os.listdir(path)
+    for file in files:  
+        pet_img = nib.load(path+file).get_fdata() # Load image
+        yield pet_img
