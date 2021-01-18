64356da6a1f23e0dd4af309f624208025800638e
Erik <erikwestberg7@gmail.com>
2021-01-18 17:07:28 +0100

    Initial Preprocessing
    Initial code for pre-processing scans, and to visualise this process.

---
+++ Preprocessing/SPM/batch_normalize_job.m
@@ -0,0 +1,78 @@
+%-----------------------------------------------------------------------
+% Job saved on 22-Sep-2020 15:00:36 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'.\TPM.nii'};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+                                                             78 76 85];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'w';
+matlabbatch{2}.spm.spatial.preproc.channel.vols(1) = cfg_dep('Normalise: Estimate & Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{2}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{2}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{2}.spm.spatial.preproc.channel.write = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(1).tpm = {'.\TPM.nii,1'};
+matlabbatch{2}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{2}.spm.spatial.preproc.tissue(1).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(2).tpm = {'.\TPM.nii,2'};
+matlabbatch{2}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{2}.spm.spatial.preproc.tissue(2).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(3).tpm = {'.\TPM.nii,3'};
+matlabbatch{2}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{2}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(4).tpm = {'.\TPM.nii,4'};
+matlabbatch{2}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{2}.spm.spatial.preproc.tissue(4).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(5).tpm = {'.\TPM.nii,5'};
+matlabbatch{2}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{2}.spm.spatial.preproc.tissue(5).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(6).tpm = {'.\TPM.nii,6'};
+matlabbatch{2}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{2}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{2}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{2}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{2}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{2}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{2}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{2}.spm.spatial.preproc.warp.write = [0 0];
+matlabbatch{2}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{2}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
+matlabbatch{3}.spm.util.imcalc.input(1) = cfg_dep('Normalise: Estimate & Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{3}.spm.util.imcalc.input(2) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+matlabbatch{3}.spm.util.imcalc.input(3) = cfg_dep('Segment: c2 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','c', '()',{':'}));
+matlabbatch{3}.spm.util.imcalc.input(4) = cfg_dep('Segment: c3 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{3}, '.','c', '()',{':'}));
+matlabbatch{3}.spm.util.imcalc.output = '';
+matlabbatch{3}.spm.util.imcalc.outdir = {'./spm_outdir'};
+matlabbatch{3}.spm.util.imcalc.expression = 'i1.*((i2+i3+i4)>0.5)';
+matlabbatch{3}.spm.util.imcalc.var = struct('name', {}, 'value', {});
+matlabbatch{3}.spm.util.imcalc.options.dmtx = 0;
+matlabbatch{3}.spm.util.imcalc.options.mask = 0;
+matlabbatch{3}.spm.util.imcalc.options.interp = 1;
+matlabbatch{3}.spm.util.imcalc.options.dtype = 4;
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(1) = cfg_dep('Normalise: Estimate & Write: Deformation (Subj 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','def'));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(2) = cfg_dep('Normalise: Estimate & Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(3) = cfg_dep('Segment: Seg Params', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','param', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(4) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(5) = cfg_dep('Segment: c2 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','c', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(6) = cfg_dep('Segment: c3 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{3}, '.','c', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(7) = cfg_dep('Segment: c4 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{4}, '.','c', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.files(8) = cfg_dep('Segment: c5 Images', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{5}, '.','c', '()',{':'}));
+matlabbatch{4}.cfg_basicio.file_dir.file_ops.file_move.action.delete = false;
