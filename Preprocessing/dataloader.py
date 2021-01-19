4940c4470fcca21bb5dfb47cde51af6a834331bc
Erik <erikwestberg7@gmail.com>
2021-01-19 10:48:18 +0100

    Improved visualisation flexibility
    Increased control of subject ID to plot, and better structured plots.

--- Preprocessing/dataloader.py
+++ Preprocessing/dataloader.py
@@ -12,3 +12,9 @@ def load_adni_data(path) -> iter:
     for file in files:  
         pet_img = nib.load(path+file).get_fdata() # Load image
         yield pet_img.T[0]
+        
+def load_spm_file(path):
+    return nib.load(path).get_fdata().T
+
+def load_adni_file(path):
+    return nib.load(path).get_fdata().T[0]
