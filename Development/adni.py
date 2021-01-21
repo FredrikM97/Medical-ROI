5ea59c20430f35c1f629ced7ce61a57f1214c023
Fredrik <Fredrik9779@gmail.com>
2021-01-21 16:41:30 +0100

    Implemented methods for adni dataset. Load, save, get, convert and more
    #fixes #10

--- Development/adni.py
+++ Development/adni.py
@@ -1,42 +1,96 @@
 import os
 import xml.etree.ElementTree as ET
 import nibabel as nib
+import pandas as pd
+import utils
+import numpy as np
+from display import display_dict_to_yaml
+import enum
+    
+class ADNI_PATHS:
+    processed=None
+    raw=None
+    category=None
+    meta=None
 
-class ADNI:
-    columns = [
+    def update_paths(self,rootpath):
+        self.processed= rootpath+'/processed/'
+        self.raw=rootpath+'/adni_raw/'
+        self.category={
+            'root':rootpath+'/adni/',
+            'AD':rootpath+'/adni/' + 'AD/',
+            'CN':rootpath+'/adni/' + 'CN/',
+            'MCI':rootpath+'/adni/' + 'MCI/',
+        }
+        self.meta=rootpath+'/meta/'
+        
+class ADNI_PROPERTIES:
+    columns:list = [
+            'projectIdentifier', #ADNI
+            'subject.subjectIdentifier', #subjectIdentifier
+            'subject.study.series.modality', #PT
+            'subject.study.imagingProtocol.description', #ADNI_Brain_PET__Raw_FDG
+            'subject.study.series.dateAcquiredPrecise', #20120120110538472
+            'image_nbr',#374
+            'series',#S137674
+            'subject.study.imagingProtocol.imageUID',
+            'filename',
+            'path',
+        ]
+    """
+    columns:list = [
         'projectIdentifier',
         'subject.subjectIdentifier',
         'ImageProtocol.description',
-        'dateAcquired',
+        'subject.study.series.dateAcquiredPrecise',
         'subject.study.imagingProtocol.imageUID', 
         'filename',
         'path'
     ]
-    projectIdentifier = 'ADNI'
-    files = []
-    meta = []
+    """
+    projectIdentifier:str = 'ADNI'
+    files:list = []
+    meta:list = []
+    category_cols = ['subject.researchGroup','subject.subjectIdentifier','subject.study.imagingProtocol.imageUID','image_nbr']
+    path = ADNI_PATHS()
     
+class ADNI(ADNI_PROPERTIES):
     
     def __init__(self, root_dir):
-        self.preprocessed_image_dir = root_dir + '/processed/'
-        self.raw_image_dir = root_dir + '/adni_raw/'
-        self.image_dir = root_dir + '/adni/'
-        self.meta_dir = root_dir + '/meta/'
-    
-    def load_meta(self, path=None) -> iter:
-        "Load meta to list. Creates a iterator"
-        if not path: path=self.meta_dir
-        self.meta = [self.__xml_to_dict(root.findall('./*')[0], delimiter='') for root in self.__load_xml(path)]
+        self.path.update_paths(root_dir)
         
-    def load_files(self,path=None) -> iter:
+    def load_meta(self, path=None, show_output=True) -> iter:
+        "Load meta to list. Creates a iterator"
+        path = path if path else self.path.meta
+        files = [self.__xml_to_dict(root.findall('./*')[0], delimiter='') for root in self.__load_xml(path)]
+        if show_output: print(f'Root path: {path}\nLoaded files: {len(files)}')
+        return files
+        
+    def load_files(self,path=None, columns=None, show_output=True) -> iter:
         "Load image paths from image_dir"
-        if not path: path=self.raw_image_dir
-        contents = []
+        (path,columns,func) = (path,columns,self.info_from_custom_filename) if path and columns else (self.path.raw,self.columns,self.info_from_raw_filename)
+        #if not (path and columns): path=self.raw_image_dir; columns=self.columns
+        
         start = path.rfind(os.sep) + 1
         
         getFolder = lambda path: path[start:].split(os.sep)
         
-        self.files =[
+        files =  [
+            dict(
+                zip(
+                    columns,[
+                        *func(file), file, path+"/"+file
+                    ]
+                )
+            ) 
+            for path, dirs, files in os.walk(path) 
+            for file in files if file.endswith('.nii')
+        ]
+        if show_output: print(f"Root path: {path}\nLoaded files: {len(files)}\nColumns:\n\t" + '\n\t'.join(columns))
+        return files
+    
+        """
+        return [
             dict(
                 zip(
                     self.columns,[
@@ -47,64 +101,188 @@ class ADNI:
             for path, dirs, files in os.walk(path) 
             for file in files if file.endswith('.nii')
         ]
-    def load(self):
+        """
+    def load(self,show_output=True):
         "Load both images and meta"
-        self.load_files()
-        self.load_meta()
-    def get_files(self, files=None) -> iter:
-        for file in files:
-            yield file
+        self.files = self.load_files(show_output=show_output)
+        self.meta = self.load_meta(show_output=show_output)
+    
+    def info_from_custom_filename(self, file, sep='#'):
+        slices= file.split(sep)
+        slices[-1] = slices[-1].split(".")[0]
+        #slices.append()
+        return slices
+    
+    def info_from_raw_filename(self,file):
+        "Get all info from filename instead (bit slower and could do wrong but removes need of multiple folders)"
+        split_order = [('_',1),('_',3),('_',1),('_br_raw_',1),('_',1),('_',1),('_',1),('.',1)] 
+        c =[]
+        def split(strng, sep, pos):
+            strng = strng.split(sep)
+            return sep.join(strng[:pos]), sep.join(strng[pos:])
+        i = file
+        for s in split_order:
+            e,i = split(i, s[0],s[1])
+            c.append(e)
+        return c
+        
+    def get_files(self, path=None,columns=None) -> iter:
+        "Get files from class"
+        files = self.load_files(path=path,columns=columns) if path and columns else self.files  
+        #files = self.load_files(path=path) if path is not None else self.files    
+        
+        def _generator():
+            for file in files:
+                yield file
+                
+        return _generator()
             
     def get_images(self, files=None) -> iter:
-        if not files: files = self.files
-        #ADNI_002_S_0295_PT_ADNI_Brain_PET__Raw_FDG_br_raw_20110609102421118_60_S111104_I239487.nii
-        for file in files:
-            yield nib.load(file['path'])
+        "Load image file into memory"
+        files = files if files else self.files
+        #if not files: files = self.files
+        
+        def _generator():
+            for file in files:
+                yield self.get_image(file['path'])
+                
+        return _generator()
+        
     
     def get_metas(self) -> iter:
+        "Get metdata"
         for meta in self.meta:
             yield meta
     
+    def get_dataset(self, dist:list=[0.6, 0.15]):
+        "Load dataset and assign labels. Double check that labels always are the same!"
+        
+        df = self.to_df()
+        df["labels"] = df["subject.researchGroup"].cat.codes
+        dataset = df[['path', 'labels']]
+
+        DATASET_SIZE = len(df)
+        train, validate, test = np.split(dataset.sample(frac=1), [int(dist[0]*DATASET_SIZE), int((1-dist[1])*DATASET_SIZE)])
+        print(f"Dataset sizes:\n\tTrain: {len(train)}({dist[0]})\n\tValidation: {len(validate)}({dist[1]})\n\tTest: {len(test)}({dist[1]})\n\tTotal: {DATASET_SIZE}")
+        return train, validate, test
+        
+    def get_dataset_images(self):
+        pass
+    
     def to_slices(self, image_list=None) -> iter:
-        if not image_list: image_list = self.get_images()
-        for image in self.to_array():
-            for slices in image:
-                for one_slice in slices:
-                    print("derp",one_slice.shape,slices.shape)
-                    yield one_slice
+        "Get each slice from each image in path"
+        image_list = image_list if image_list else self.get_images()
+        #if not image_list: image_list = self.get_images()
+        
+        def _generator():
+            for image in self.to_array(image_list=image_list):
+                for slices in image:
+                    for one_slice in slices:
+                        print("derp",one_slice.shape,slices.shape)
+                        yield one_slice
+                
+        return _generator()
+    
+        
             
     def to_array(self, image_list=None) -> iter:
-        if not image_list: image_list = self.get_images()
+        "Convert images into numpy arrays and transpose from (x,y,z,n) -> (n,z,x,y)"
+        image_list = image_list if image_list else self.get_images()
+        #if not image_list: image_list = self.get_images()
         for file in image_list:
             yield file.get_fdata().T
             
-    def to_df(self):
+    def to_df(self, show_output=True):
         "Convert image list and meta list to "
-        image_df = pd.DataFrame(list(self.get_files()),self.columns)
+        files_df = self.files_to_df(show_output=show_output)
+        meta_df = self.meta_to_df(show_output=show_output)        
         
-        meta_df = pd.DataFrame(list(self.meta)).sort_values('subject.subjectIdentifier')
+        df = self.__merge_df(files_df,meta_df)
+        return df
+    
+    def meta_to_df(self, show_output=True):
+        "Convert metadata list to dataframe"
+        meta_df = pd.DataFrame(list(self.get_metas())).sort_values('subject.subjectIdentifier')
         # Add I so that images and meta is named the same!
         meta_df['subject.study.imagingProtocol.imageUID'] = 'I' + meta_df['subject.study.imagingProtocol.imageUID']
         
-        df = self.__merge_df(image_df,meta_df)
-        return df
+        meta_df = utils.convert_df_types(meta_df, types={
+            'num':[
+                'subject.study.subjectAge',
+                'subject.study.weightKg',
+                'subject.visit.assessment.component.assessmentScore_MMSCORE',
+                'subject.visit.assessment.component.assessmentScore_GDTOTAL',
+                'subject.visit.assessment.component.assessmentScore_CDGLOBAL',
+                'subject.visit.assessment.component.assessmentScore_NPISCORE',
+                'subject.visit.assessment.component.assessmentScore_FAQTOTAL'
+            ],
+            'cat':[
+                'subject.researchGroup'
+            ],
+            'str':[
+                'subject.subjectSex'
+            ],
+            'datetime':[
+                'subject.study.series.dateAcquired'
+            ]
+        }, show_output=show_output)
+        return meta_df
+    
+    def files_to_df(self, show_output=True):
+        "Convert files list to dataframe"
+        files_df = pd.DataFrame(list(self.get_files()),columns=self.columns)
+        
+        files_df = utils.convert_df_types(files_df, types={},show_output=show_output)
+        return files_df
     
     def get_image(self, path):
+        "Load one image"
         return nib.load(path)
     
     def get_meta(self, path):
-        return 
+        "Get metadata from path"
+        return self.load_meta(path)
+    
     def nii_header(self,image_obj):
+        "Get header from image object"
         return image_obj.header
     
-    def save_to_category(self,output_df, path=None)->None:
+    def save_to_category(self,output_df, path:dict=None, show_output=True)->None:
         "Save images to categories based on parameters from dataframe"
-        if not path: path=self.image_dir
-        output_df.apply(lambda row: copy_file(str(row['path']), f"{path}/{row['subject.researchGroup']}/{row['filename']}"), axis=1)
+        path = path if path else self.path.category
         
+        if show_output: print(f"Copy files: \n\t"+ "\n\t".join([f'{key}: {value}' for key, value in path.items()]))
+        stats = {
+            'AD':{
+                'Skip':0, # Skip
+                'Transfer':0, # Transfer
+            },
+            'CN':{
+                'Skip':0, # Skip
+                'Transfer':0, # Transfer
+            },
+            'MCI':{
+                'Skip':0, # Skip
+                'Transfer':0, # Transfer
+            }
+        }
+        conv = {
+            0:'Skip',
+            1:'Transfer'
+        }
+        def inner(row):
+            filename = f"{'#'.join([row[p] for p in self.category_cols])}.nii"
+            response = utils.copy_file(str(row['path']), f"{path[row['subject.researchGroup']]}/{filename}")
+            stats[row['subject.researchGroup']][conv[response]] += 1 
+        
+        output_df.apply(lambda row: inner(row), axis=1)
+        
+        if show_output: display_dict_to_yaml({'Statistics':stats})
+   
     
         
     def __xml_to_dict(self,r, parent='', delimiter=".") -> list:
+        "Iterate through all xml files and add them to a dictionary"
         param = lambda r:"_"+list(r.attrib.values())[0] if r.attrib else ''
         def recursive(r, parent='', delimiter=".") -> list:
             cont = {}
@@ -116,21 +294,16 @@ class ADNI:
             elif r.text and '\n' not in r.text: # get text
                 return {parent + param(r):r.text}
             else:
-                return {'dummy':None}
+                return {}
         return recursive(r, parent=parent, delimiter=delimiter)
     
     def __load_xml(self, path:str):
         "Load XML from dictory and return a generator"
         assert path, "No path defined"
-        content = []
         for filename in os.listdir(path):
             if not filename.endswith('.xml'): continue
             fullname = os.path.join(path, filename)
-            yield self.__get_xml(fullname)
-            
-    def __get_xml(self, path):
-        return ET.parse(path)
-            
+            yield ET.parse(fullname)
             
     def __merge_df(self,image_df,meta_df, cols=['subject.subjectIdentifier','subject.study.imagingProtocol.imageUID']):
         """Merge two dataframes based on 'subject.subjectIdentifier' and 'subject.study.imagingProtocol.imageUID'"""
--- Development/dataloader.py
+++ Development/dataloader.py
@@ -1,11 +1,5 @@
 import os
-import shutil
+
         
-def copy_file(src, dest):
-    "Copy file from source dir to dest dir. Note that the path must exist where folders should be placed!"
-    assert os.path.exists(src), "Source file does not exists"
-    
-    if not os.path.exists(dest):
-        shutil.copy(src, dest)
-        return 'ok' 
-    return "fail"
+
+
