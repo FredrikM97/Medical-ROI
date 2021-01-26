60c45c111461c0cfbe65d1c12df697db9c5d3fb3
Fredrik <Fredrik9779@gmail.com>
2021-01-26 15:30:26 +0100

    Added multiple plot functions based on seaborn

--- Development/utils/display.py
+++ Development/utils/display.py
@@ -4,6 +4,7 @@ from ruamel.yaml import YAML
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from math import ceil,floor
+import seaborn as sns
 
 def display_all_pd_cols(input_df):
     with pd.option_context('display.max_columns', None):
@@ -44,3 +45,96 @@ def display_advanced_plot(slices):
         ax.label_outer()
 
     plt.show()  # finally, render the plot
+
+def move_legend(ax, new_loc="upper left",title:str=None,**kws):
+    """
+    If the legend disapear this function should be used. 
+    Recommended from https://github.com/mwaskom/seaborn/issues/2280 where matplotlib have some weird implementation which makes the legend disappear..
+    """
+    old_legend = ax.legend_
+    handles = old_legend.legendHandles
+    labels = [t.get_text() for t in old_legend.get_texts()]
+    if not title: title = old_legend.get_title().get_text() #, loc=new_loc
+    ax.legend(handles, labels, title=title, borderaxespad=0., bbox_to_anchor=(1.02, 1), loc=new_loc,**kws)
+
+def set_plot_settings(ax,rotation:int=0, title:str=None, xlabel:str=None, ylabel:str=None, **kws):
+    if title: ax.set_title(title)
+    if xlabel and ylabel: ax.set(xlabel=xlabel, ylabel=ylabel)
+    
+    ax.tick_params(axis='x',which='major', rotation=rotation)
+    
+    
+def do_countplot(
+                data, 
+                x:str=None, 
+                y:str=None, 
+                hue:str=None, 
+                order:list=None, 
+                ax=None,
+                plot_kws:dict={},
+                setting_kws:dict={},
+                legend_kws:dict={},
+                **kws
+            ):
+    """
+    Example:
+    dplay.do_countplot(
+        df, 
+        x="subject.visit.assessment.component.assessmentScore_FAQTOTAL", 
+        hue="subject.researchGroup", 
+        order=np.arange(1,31), 
+        rotation=90, 
+        xlabel="FAQTOTAL", 
+        ylabel="Frequency",
+        title="Functional Activities Questionnaires (FAQTOTAL)",
+        ax=axes[0,0]
+    )
+    """
+    sns.set(font_scale=1.1)
+
+    ax1 = sns.countplot(x=x,y=y,
+                hue = hue, 
+                data = data,
+                order=order,
+                ax=ax,
+                **plot_kws
+            )
+
+    set_plot_settings(ax1,**setting_kws)
+    move_legend(ax1,**legend_kws)
+    plt.tight_layout()
+    return ax1
+
+def do_histplot(
+            df, 
+            x=None,
+            y=None,
+            hue:str=None, 
+            title:str=None,
+            multiple='stack',
+            bins:int='auto',
+            discrete=True,
+            ax=None,
+            plot_kws:dict={},
+            setting_kws:dict={},
+            legend_kws:dict={},
+            **kws
+        ):
+    sns.set(font_scale=1.1)
+    print(bins)
+    ax1 = sns.histplot(
+        df,
+        x=x,
+        y=y, 
+        hue=hue,
+        bins=bins,
+        discrete=discrete,
+        multiple=multiple,
+        ax=ax,
+        **plot_kws,
+        
+    )
+    set_plot_settings(ax1,**setting_kws)
+    move_legend(ax1,**legend_kws)
+    plt.tight_layout()
+    return ax1
