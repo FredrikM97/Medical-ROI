daf479c22e1e595c8d18b822821e2162f70484be
Fredrik <fredrik9779@gmail.com>
2021-01-28 15:29:37 +0100

    Fixed assert error

--- Development/utils/misc_util.py
+++ Development/utils/misc_util.py
@@ -57,7 +57,7 @@ def convert_df_types(
 
 def copy_file(src, dest)-> bool:
     "Copy file from source dir to dest dir. Note that the path must exist where folders should be placed!"
-    assert os.path.exists(src), "Source file does not exists"
+    assert not os.path.exists(src), "Source file does not exists"
     
     if not os.path.exists(dest):
         shutil.copy(src, dest)
