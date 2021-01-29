84dc0a7cd506700c7d41ec7b9bcf20fdebe5a9fb
Fredrik <fredrik9779@gmail.com>
2021-01-30 00:25:20 +0100

    Split data into functions for easier reading

--- Development/neural_network/train.py
+++ Development/neural_network/train.py
@@ -5,6 +5,7 @@ import math
 from models import create_model
 import time
 from utils.visualizer import Visualizer
+import multiprocessing
 
 """Performs training of a specified model.
     
@@ -15,58 +16,77 @@ Input params:
         model-specific settings.
     export: Whether to export the final model (default=True).
 """
-def train(config_file, export=True):
-    print('Reading config file...')
-    configuration = parse_configuration(config_file)
-
+def init_dataset(config):
     print('Initializing dataset...')
-    train_dataset = create_dataset(configuration['train_dataset_params'])
-    train_dataset_size = len(train_dataset)
-    print('The number of training samples = {0}'.format(train_dataset_size))
+    train_dataset = create_dataset(config['train_dataset_params'])
+    val_dataset = create_dataset(config['val_dataset_params'])
+    
+    print('The number of samples:\
+          \n\tTraining: {0}\
+          \n\tValidation:{1}\
+          '.format(len(train_dataset),len(val_dataset)))
+    return train_dataset, val_dataset
 
-    val_dataset = create_dataset(configuration['val_dataset_params'])
-    val_dataset_size = len(val_dataset)
-    print('The number of validation samples = {0}'.format(val_dataset_size))
-
+def init_model(config):
     print('Initializing model...')
-    model = create_model(configuration['model_params'])
+    model = create_model(config['model_params'])
     model.setup()
+    
+    return model
 
+def init_visualize(config):
     print('Initializing visualization...')
-    visualizer = Visualizer(configuration['visualization_params'])   # create a visualizer that displays images and plots
+    return Visualizer(config['visualization_params'])   # create a visualizer that displays images and plots
 
-    starting_epoch = configuration['model_params']['load_checkpoint'] + 1
-    num_epochs = configuration['model_params']['max_epochs']
+def init_train(config):
+    starting_epoch = config['model_params']['load_checkpoint'] + 1
+    num_epochs = config['model_params']['max_epochs']
+    
+    return starting_epoch, num_epochs
 
+def train(config_file, export=True):
+    multiprocessing.set_start_method('spawn', True)
+
+    print('Reading config file...')
+    config = parse_configuration(config_file)
+    train_dataset, val_dataset = init_dataset(config)
+    model = init_model(config)
+    visualizer = init_visualize(config)
+    starting_epoch, num_epochs = init_train(config)
+    
+    print("Start training...")
     for epoch in range(starting_epoch, num_epochs):
         epoch_start_time = time.time()  # timer for entire epoch
         train_dataset.dataset.pre_epoch_callback(epoch)
         model.pre_epoch_callback(epoch)
 
         train_iterations = len(train_dataset)
-        train_batch_size = configuration['train_dataset_params']['loader_params']['batch_size']
-
+        train_batch_size = config['train_dataset_params']['loader_params']['batch_size']
+        
+        # Train model
         model.train()
         for i, data in enumerate(train_dataset):  # inner loop within one epoch
             visualizer.reset()
-
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.forward()
             model.backward()
 
-            if i % configuration['model_update_freq'] == 0:
+            if i % config['model_update_freq'] == 0:
                 model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
-            if i % configuration['printout_freq'] == 0:
+            if i % config['printout_freq'] == 0:
                 losses = model.get_current_losses()
                 visualizer.print_current_losses(epoch, num_epochs, i, math.floor(train_iterations / train_batch_size), losses)
                 visualizer.plot_current_losses(epoch, float(i) / math.floor(train_iterations / train_batch_size), losses)
-
+        
+        # Validate model
         model.eval()
         for i, data in enumerate(val_dataset):
             model.set_input(data)
             model.test()
-
+        
+        
+        
         model.post_epoch_callback(epoch, visualizer)
         train_dataset.dataset.post_epoch_callback(epoch)
 
@@ -74,22 +94,24 @@ def train(config_file, export=True):
         model.save_networks(epoch)
         model.save_optimizers(epoch)
 
-        print('End of epoch {0} / {1} \t Time Taken: {2} sec'.format(epoch, num_epochs, time.time() - epoch_start_time))
+        print('End of epoch {0} / {1} \t Time Taken: {2} sec'.format(epoch, num_epochs, time.time() - epoch_start_time), sep="\r")
 
         model.update_learning_rate() # update learning rates every epoch
 
     if export:
         print('Exporting model')
         model.eval()
-        custom_configuration = configuration['train_dataset_params']
-        custom_configuration['loader_params']['batch_size'] = 1 # set batch size to 1 for tracing
-        dl = train_dataset.get_custom_dataloader(custom_configuration)
+        custom_config = config['train_dataset_params']
+        custom_config['loader_params']['batch_size'] = 1 # set batch size to 1 for tracing
+        dl = train_dataset.get_custom_dataloader(custom_config)
         sample_input = next(iter(dl)) # sample input from the training dataset
         model.set_input(sample_input)
         model.export()
 
     return model.get_hyperparam_result()
 
+    
+"""
 if __name__ == '__main__':
     import multiprocessing
     multiprocessing.set_start_method('spawn', True)
@@ -99,3 +121,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     train(args.configfile)
+"""
