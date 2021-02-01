35f6b663d34189cf97cf076dbd265fe91f184ecb
Fredrik <fredrik9779@gmail.com>
2021-02-01 15:59:17 +0100

    Moved functions to Agent class in order to improve readability

--- Development/neural_network/train.py
+++ Development/neural_network/train.py
@@ -1,5 +1,6 @@
 import argparse
 from datasets import create_dataset
+from configs import load_config
 from utils import parse_configuration
 import math
 from models import create_model
@@ -16,100 +17,122 @@ Input params:
         model-specific settings.
     export: Whether to export the final model (default=True).
 """
-def train(config_file, export=True):
-    multiprocessing.set_start_method('spawn', True)
+class Agent:
+    def __init__(self, config_name:str, export:bool=True):
+        #multiprocessing.set_start_method('spawn', True)
+        # Setup config
+        print('Setup configurations...')
+        self.config = load_config(config_name)
+        
+        # Setup model
+        self.model = create_model(self.config['model_params'])
+        self.model.setup()
+        
+        # Setup dataloader
+        self.train_dataset = create_dataset(self.config['train_dataset_params'])
+        self.val_dataset = create_dataset(self.config['val_dataset_params'])
+        
+        # Setup visualisation
+        self.visualizer = Visualizer(self.config['visualization_params'])   # create a visualizer that displays images and plots
+        
+        
+        # Other meta
 
-    print('Setup configurations...')
-    config = parse_configuration(config_file)
-    
-    # Setup model
-    model = create_model(config['model_params'])
-    model.setup()
-    
-    # Setup dataloader
-    train_dataset = create_dataset(config['train_dataset_params'])
-    val_dataset = create_dataset(config['val_dataset_params'])
-    
-    print('The number of samples:\
-          \n\tTraining: {0}\
-          \n\tValidation:{1}\
-          '.format(len(train_dataset),len(val_dataset)))
-    
-    # Setup visualisation
-    visualizer = Visualizer(config['visualization_params'])   # create a visualizer that displays images and plots
-    
-    # Setup epochs 
-    starting_epoch = config['model_params']['load_checkpoint'] + 1
-    num_epochs = config['model_params']['max_epochs']
-    
-    # Setup meta
-    print_freq = config['printout_freq']
-    model_update_freq = config['model_update_freq']
-    # Init meta
-    steps = 0
-    running_loss = 0
-    
+    def fit(self):
+        # Setup epochs 
+        starting_epoch = self.config['model_params']['load_checkpoint'] + 1
+        num_epochs = self.config['model_params']['max_epochs']
 
-    print("Start training...")
-    # Run through dataset num_epochs number of times.
-    for epoch in range(starting_epoch, num_epochs):
-        epoch_start_time = time.time()  # timer for entire epoch
-        train_dataset.dataset.pre_epoch_callback(epoch)
-        model.pre_epoch_callback(epoch)
+        # Setup meta
+        print_freq = self.config['printout_freq']
+        model_update_freq = self.config['model_update_freq']
+        # Init meta
+        running_loss = 0
 
+
+        print("Start training...")
+        # Run through dataset num_epochs number of times.
+        for epoch in range(starting_epoch, num_epochs):
+            epoch_start_time = time.time()  # timer for entire epoch
+            self.pre_epoch_callback(epoch)
+
+
+            # Train model
+            self.model.train()
+            for step, data in enumerate(self.train_dataset,0):  # inner loop within one epoch
+                print("type", data[0].type(), data[1].type())
+                self.train_step(data)
+                print(f"Step: {step}", sep="\r")
+            
+            # Evaluate model
+            self.model.eval()
+            for step, data in enumerate(self.val_dataset):
+                self.test_step(data)
+    
+            
+            self.post_epoch_callback(epoch)
+
+            print('Saving model at the end of epoch {0}'.format(epoch))
+            self.model.save_networks(epoch)
+            self.model.save_optimizers(epoch)
+
+            print('End of epoch {0} / {1} \t Time Taken: {2} sec'.format(epoch, num_epochs, time.time() - epoch_start_time), sep="\r")
+
+            self.model.update_learning_rate() # update learning rates every epoch
+        """
+        if export:
+            print('Exporting model')
+            model.eval()
+            custom_config = config['train_dataset_params']
+            custom_config['loader_params']['batch_size'] = 1 # set batch size to 1 for tracing
+            dl = train_dataset.get_custom_dataloader(custom_config)
+            sample_input = next(iter(dl)) # sample input from the training dataset
+            model.set_input(sample_input)
+            model.export()
+        """
+
+        #return self.model.get_hyperparam_result()
+
+    def train_step(self, data):
+        "Enumerate one item of mini batch"
+        self.model.set_input(data)         # unpack data from dataset and apply preprocessing
+        self.model.forward()
+        self.model.backward()
+        print("Do I even get here?")
+        #if i % model_update_freq == 0:
+        self.model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
+        
+        return model.get_current_losses()
+
+    def test_step(self, data):
+        "Enumerate one item of mini batch"
+        # Validate model
+        self.model.set_input(data)
+        self.model.test()
+    
+    def pre_epoch_callback(self, epoch):
+        self.train_dataset.dataset.pre_epoch_callback(epoch)
+        self.model.pre_epoch_callback(epoch)
+    
+    def post_epoch_callback(self, epoch):
+        self.model.post_epoch_callback(epoch, self.visualizer)
+        self.train_dataset.dataset.post_epoch_callback(epoch)
+        
+    """
+    def some_odd_visualizer_function(self):
+        print('The number of samples:\
+              \n\tTraining: {0}\
+              \n\tValidation:{1}\
+              '.format(len(self.train_dataset),len(self.val_dataset)))
+              
         #train_iterations = len(train_dataset)
         #train_batch_size = config['train_dataset_params']['loader_params']['batch_size']
-        
-        # Train model
-        model.train()
-        for i, data in enumerate(train_dataset,0):  # inner loop within one epoch
-            steps += 1
-            inputs, labels = data
-            print("type of ", type(inputs), type(labels))
-            #visualizer.reset()
-            model.set_input(data)         # unpack data from dataset and apply preprocessing
-            model.forward()
-            model.backward()
-
-            if i % model_update_freq == 0:
-                model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
-
-            if i % print_freq == 0:
-                losses = model.get_current_losses()
-                #visualizer.print_current_losses(epoch, num_epochs, i, math.floor(train_iterations / train_batch_size), losses)
-                #visualizer.plot_current_losses(epoch, float(i) / math.floor(train_iterations / train_batch_size), losses)
-        
-        # Validate model
-        model.eval()
-        for i, data in enumerate(val_dataset):
-            model.set_input(data)
-            model.test()
-        
-        
-        
-        model.post_epoch_callback(epoch, visualizer)
-        train_dataset.dataset.post_epoch_callback(epoch)
-
-        print('Saving model at the end of epoch {0}'.format(epoch))
-        model.save_networks(epoch)
-        model.save_optimizers(epoch)
-
-        print('End of epoch {0} / {1} \t Time Taken: {2} sec'.format(epoch, num_epochs, time.time() - epoch_start_time), sep="\r")
-
-        model.update_learning_rate() # update learning rates every epoch
-
-    if export:
-        print('Exporting model')
-        model.eval()
-        custom_config = config['train_dataset_params']
-        custom_config['loader_params']['batch_size'] = 1 # set batch size to 1 for tracing
-        dl = train_dataset.get_custom_dataloader(custom_config)
-        sample_input = next(iter(dl)) # sample input from the training dataset
-        model.set_input(sample_input)
-        model.export()
-
-    return model.get_hyperparam_result()
-
+        #visualizer.reset()
+        if i % print_freq == 0:
+            losses = model.get_current_losses()
+            #visualizer.print_current_losses(epoch, num_epochs, i, math.floor(train_iterations / train_batch_size), losses)
+            #visualizer.plot_current_losses(epoch, float(i) / math.floor(train_iterations / train_batch_size), losses)
+    """
     
 """
 if __name__ == '__main__':
