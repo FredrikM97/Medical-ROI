9d1c044e775fc4954af7f6022ee92a084d42c4b9
Fredrik <fredrik9779@gmail.com>
2021-02-01 12:27:00 +0100

    Improved performance and added label encoder

--- Development/neural_network/datasets/segmentation2d_dataset.py
+++ Development/neural_network/datasets/segmentation2d_dataset.py
@@ -1,6 +1,6 @@
 from datasets.base_dataset import get_transform
 from datasets.base_dataset import BaseDataset
-from utils.utils import get_nii_files
+from utils.utils import get_nii_files, label_encoder
 import torch
 import os
 import numpy as np
@@ -14,16 +14,21 @@ class Segmentation2DDataset(BaseDataset):
     """
     def __init__(self, config):
         super().__init__(config)
+        # This is probobly really bad but lets roll with it.
+        predefined_labels = {'MCI':0,'AD':1,'CN':2}
         #self.img_files = os.listdir(config['dataset_path'])
         self.img_files = get_nii_files(config['dataset_path'])
+        self.img_labels = label_encoder([img_path.rsplit("/",1)[1].split("#",1)[0] for img_path in self.img_files])
+        
         
     def __getitem__(self, idx):
         img_path = self.img_files[idx]
         
         img = nib.load(img_path)
         x = torch.from_numpy(img.get_fdata())
-        y = img_path.rsplit("/",1)[1].split("#",1)[0]
-
+        y = self.img_labels[idx]
+        #y = img_path.rsplit("/",1)[1].split("#",1)[0]
+        print("What numpy type am I ", type(x), type(y))
         return x, y
 
     def __len__(self):
