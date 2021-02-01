6df4643941ac3fc528678f3c29ccfefb9c882363
Fredrik <fredrik9779@gmail.com>
2021-02-01 12:26:32 +0100

    Reduce bloated code and cleanup

--- Development/neural_network/train.py
+++ Development/neural_network/train.py
@@ -16,8 +16,17 @@ Input params:
         model-specific settings.
     export: Whether to export the final model (default=True).
 """
-def init_dataset(config):
-    print('Initializing dataset...')
+def train(config_file, export=True):
+    multiprocessing.set_start_method('spawn', True)
+
+    print('Setup configurations...')
+    config = parse_configuration(config_file)
+    
+    # Setup model
+    model = create_model(config['model_params'])
+    model.setup()
+    
+    # Setup dataloader
     train_dataset = create_dataset(config['train_dataset_params'])
     val_dataset = create_dataset(config['val_dataset_params'])
     
@@ -25,59 +34,50 @@ def init_dataset(config):
           \n\tTraining: {0}\
           \n\tValidation:{1}\
           '.format(len(train_dataset),len(val_dataset)))
-    return train_dataset, val_dataset
-
-def init_model(config):
-    print('Initializing model...')
-    model = create_model(config['model_params'])
-    model.setup()
     
-    return model
-
-def init_visualize(config):
-    print('Initializing visualization...')
-    return Visualizer(config['visualization_params'])   # create a visualizer that displays images and plots
-
-def init_train(config):
+    # Setup visualisation
+    visualizer = Visualizer(config['visualization_params'])   # create a visualizer that displays images and plots
+    
+    # Setup epochs 
     starting_epoch = config['model_params']['load_checkpoint'] + 1
     num_epochs = config['model_params']['max_epochs']
     
-    return starting_epoch, num_epochs
-
-def train(config_file, export=True):
-    multiprocessing.set_start_method('spawn', True)
-
-    print('Reading config file...')
-    config = parse_configuration(config_file)
-    train_dataset, val_dataset = init_dataset(config)
-    model = init_model(config)
-    visualizer = init_visualize(config)
-    starting_epoch, num_epochs = init_train(config)
+    # Setup meta
+    print_freq = config['printout_freq']
+    model_update_freq = config['model_update_freq']
+    # Init meta
+    steps = 0
+    running_loss = 0
     
+
     print("Start training...")
+    # Run through dataset num_epochs number of times.
     for epoch in range(starting_epoch, num_epochs):
         epoch_start_time = time.time()  # timer for entire epoch
         train_dataset.dataset.pre_epoch_callback(epoch)
         model.pre_epoch_callback(epoch)
 
-        train_iterations = len(train_dataset)
-        train_batch_size = config['train_dataset_params']['loader_params']['batch_size']
+        #train_iterations = len(train_dataset)
+        #train_batch_size = config['train_dataset_params']['loader_params']['batch_size']
         
         # Train model
         model.train()
-        for i, data in enumerate(train_dataset):  # inner loop within one epoch
-            visualizer.reset()
+        for i, data in enumerate(train_dataset,0):  # inner loop within one epoch
+            steps += 1
+            inputs, labels = data
+            print("type of ", type(inputs), type(labels))
+            #visualizer.reset()
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.forward()
             model.backward()
 
-            if i % config['model_update_freq'] == 0:
+            if i % model_update_freq == 0:
                 model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
-            if i % config['printout_freq'] == 0:
+            if i % print_freq == 0:
                 losses = model.get_current_losses()
-                visualizer.print_current_losses(epoch, num_epochs, i, math.floor(train_iterations / train_batch_size), losses)
-                visualizer.plot_current_losses(epoch, float(i) / math.floor(train_iterations / train_batch_size), losses)
+                #visualizer.print_current_losses(epoch, num_epochs, i, math.floor(train_iterations / train_batch_size), losses)
+                #visualizer.plot_current_losses(epoch, float(i) / math.floor(train_iterations / train_batch_size), losses)
         
         # Validate model
         model.eval()
