f57cb05e3c84bd52721dba57b94ec51af99ecf87
Fredrik <fredrik9779@gmail.com>
2021-02-04 18:02:10 +0100

    Improved callbacks and logging

--- Development/neural_network_lightning/ml_lightning.py
+++ Development/neural_network_lightning/ml_lightning.py
@@ -1,32 +1,42 @@
 from configs import load_config
 from models import create_model
 from datasets import create_dataset
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint,ProgressBar
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 
 class Agent:
     def __init__(self, config_name:str, export:bool=True):
-        #multiprocessing.set_start_method('spawn', True)
+
         print('Setup configurations...')
+        # ****** Setup configurations ******
         self.config = load_config(config_name)
         
-        # Setup model
+        
+        # ****** Setup loggers ******
+       
+        checkpoint_callback = ModelCheckpoint(
+            #filename=self.config['model_params']['checkpoint_path']+'mnist_{epoch}-{val_loss:.2f}',
+            #monitor='val_loss', 
+            #mode='min', save_top_k=3
+        )
+        progressbar_callback = ProgressBar()
+      
+        #lr_logger = LearningRateLogger()
+        tb_logger = pl_loggers.TensorBoardLogger("logs/tb_logs", name=self.config['model_params']['model_name'])
+        
+        # ****** Setup model ******
         self.model = create_model(self.config['model_params'])
         
-        # Setup dataloader
+        # ****** Setup dataloader ******
         dataset = create_dataset(self.config['dataset_params'])
         
-        self.val_loader = dataset.train_dataloader()
-        self.train_loader = dataset.val_dataloader()
+        self.train_loader = dataset.train_dataloader()
+        self.val_loader = dataset.val_dataloader()
         
-        checkpoint_callback = ModelCheckpoint(
-            filename=self.config['model_params']['checkpoint_path']+'mnist_{epoch}-{val_loss:.2f}',
-            monitor='val_loss', 
-            mode='min', save_top_k=3
-        )
-        #lr_logger = LearningRateLogger()
-        tb_logger = pl_loggers.TensorBoardLogger('logs/')
+        print(f"Size of:\n\tTrainset: {len(self.train_loader)}\n\tValset: {len(self.val_loader)}")
+        
+        # ****** Setup trainer ******
         
         self.trainer = pl.Trainer(
             max_epochs=self.config['model_params']['max_epochs'], 
@@ -34,12 +44,12 @@ class Agent:
             #callbacks=[lr_logger], 
             #gpus=None, 
             logger=tb_logger,
-            callbacks=[checkpoint_callback],
-            default_root_dir=self.config['model_params']['checkpoint_path']
+            callbacks=[checkpoint_callback,progressbar_callback],
+            #default_root_dir=self.config['model_params']['checkpoint_path']
         ) #gpus=1
         
         
-        
+    
         
         
     def fit(self):
@@ -49,3 +59,7 @@ class Agent:
             val_dataloaders=self.val_loader
         )
     
+    def test(self):
+        trainer.test(
+            test_dataloaders=self.test_dataloader
+        )
