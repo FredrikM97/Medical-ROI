c1fbc7af881aab9ce3a697801c3edcd3569fb515
Fredrik <fredrik9779@gmail.com>
2021-02-05 21:12:49 +0100

    Fixed missing metrics

--- Development/neural_network_lightning/models/lightning_model.py
+++ Development/neural_network_lightning/models/lightning_model.py
@@ -1,7 +1,9 @@
 import pytorch_lightning as pl
 import torch.nn as nn
 import torch
+from pytorch_lightning.metrics import functional as FM
 from models.architectures import testModel
+
 class LightningModel(testModel): 
     def __init__(self, hparams):
         
@@ -17,47 +19,40 @@ class LightningModel(testModel):
         x, y = batch
         logits = self.forward(x) 
         loss = self.loss(logits, y) 
+        acc = FM.accuracy(logits, y)
         
         # log values
         #self.logger.experiment.add_scalar('Train/Loss', loss)
-        self.log('Train/Loss', loss) #, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        return {'loss': loss}
+        metrics = {'train/acc': acc, 'train/loss': loss}
+        self.log_dict(metrics)
+        return metrics
   
     def validation_step(self, batch: dict, batch_idx: int) -> dict:
         x, y = batch
         logits = self.forward(x) 
         loss = self.loss(logits, y) 
+        acc = FM.accuracy(logits, y)
         
         # log values
         #self.logger.experiment.add_scalar('Val/Loss', loss)
-        self.log('Val/Loss', loss) #, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        return {'val_loss': loss}
+        metrics = {'val/acc': acc, 'val/loss': loss}
+        self.log_dict(metrics)
+        return metrics
     
     def test_step(self, batch: dict, batch_idx: int) -> dict:
-        x, y = batch
-        logits = self.forward(x)
-        loss = self.loss(logits, y)
+        metrics = self.validation_step(batch, batch_idx)
+        metrics = {'test/acc': metrics['val/acc'], 'test/loss': metrics['val/loss']}
+        self.log_dict(metrics)
+        return metrics
         
-        # log values
-        #self.logger.experiment.add_scalar('Test/Loss', loss)
-        
-        return {'test_loss': loss}
-      
     def validation_epoch_end(self, outputs):
-        #avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        #tensorboard_logs = {'val_loss': avg_loss}
-        #self.log({'avg_val_loss': avg_loss, 'log': tensorboard_logs})
-        #self.log("val_loss", avg_loss)
+
         pass
     def test_epoch_end(self, outputs):
-        #avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        #tensorboard_logs = {'test_loss': avg_loss}
-        #self.log("test_loss", avg_loss)
-        #self.log({'avg_test_loss': avg_loss, 'log': tensorboard_logs})
+
         pass
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        lr_scheduler = {'scheduler': torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.95),
-                        'name': 'expo_lr'}
-        return [optimizer], [lr_scheduler]
+
+        return [optimizer]
