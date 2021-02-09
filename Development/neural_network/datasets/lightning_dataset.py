97e87771f6307e5edcec0dd57a646f276a9d0b82
Fredrik <fredrik9779@gmail.com>
2021-02-09 18:46:55 +0100

    Added #43 support for crossvalidation

--- Development/neural_network/datasets/lightning_dataset.py
+++ Development/neural_network/datasets/lightning_dataset.py
@@ -4,53 +4,64 @@ import torch
 import pytorch_lightning as pl 
 import nibabel as nib
 from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import KFold
+#https://www.kaggle.com/lezwon/parallel-kfold-training-on-tpu-using-pytorch-li
+#https://forums.pytorchlightning.ai/t/kfold-vs-monte-carlo-cross-validation/587
+import itertools
+import numpy as np
 
-class LightningDataset(pl.LightningModule): 
+class LightningDataset(pl.LightningDataModule): 
     
-    def __init__(self, hparams):
+    def __init__(self,**hparams:dict):
         super().__init__()
+        self._hparams = hparams
+        self.img_files = get_nii_files(self._hparams['dataset_path'])
+        assert len(self.img_files) > 0, f"No images found at path: {self._hparams['dataset_path']}"
+    
+    def setup(self,stage=None):
+        dataset = NiiDataset(self.img_files)
         
-        self.img_files = get_nii_files(hparams['dataset_path'])
-        assert len(self.img_files) > 0, f"No images found at path: {hparams['dataset_path']}"
-        
-        self.dims = hparams['input_size']
-        self.save_hyperparameters()
-        
-    def train_dataloader(self):
-        dataset = NiiDataset(train=True, data_dir=self.img_files)
+        if self._hparams['val_type'] == 'kfold':
+            print("Going with kfold!")
+            self.kfold_splits = kfold(dataset, n_splits=self._hparams['fold_splits'])
+            self.train_set, self.val_set = next(self.kfold_splits)
+        else:
+            print("Going with default split!")
+            # If not kfold then do something else...
+            num_train_samples = len(dataset)*0.9
+            self.train_set, self.val_set = (dataset[:num_train_samples], dataset[num_train_samples:])
 
-        dataloader = DataLoader(dataset,
-                                batch_size=self.hparams['hparams']['train_params']['batch_size'],
+    def _on_epoch_end(self):
+        if self._hparams['val_type'] == 'kfold':
+            self.train_set, self.val_set = next(self.trainer.datamodule.kfold_splits)
+         
+    def train_dataloader(self):
+        assert len(self.train_set) % self._hparams['train_params']['batch_size'] != 1, "A batch of size 1 is not allowed!"
+
+        dataloader = DataLoader(self.train_set,
+                                batch_size=self._hparams['train_params']['batch_size'],
                                 shuffle=True,
-                                num_workers=self.hparams['hparams']['train_params']['num_workers'])
+                                num_workers=self._hparams['train_params']['num_workers'])
         return dataloader
     
     def val_dataloader(self):
-        dataset = NiiDataset(train=False, data_dir=self.img_files)
+        assert len(self.val_set) % self._hparams['train_params']['batch_size'] != 1, "A batch of size 1 is not allowed!"
+        #dataset = NiiDataset(train=False, data_dir=self.img_files)
         
-        dataloader = DataLoader(dataset,
-                                batch_size=self.hparams['hparams']['val_params']['batch_size'],
+        dataloader = DataLoader(self.val_set,
+                                batch_size=self._hparams['val_params']['batch_size'],
                                 shuffle=False,
-                                num_workers=self.hparams['hparams']['val_params']['num_workers'])
+                                num_workers=self._hparams['val_params']['num_workers'])
 
         return dataloader
     
 class NiiDataset(Dataset):
-    def __init__(self, train: bool, data_dir: list):
-        
-        # TODO: Fix this. If data not shuffle it will always prio data in the bottom!
-        num_train_samples = int(len(data_dir) * 0.9)
-        
-        if train:
-            data = data_dir[:num_train_samples]
-        else:
-            data = data_dir[num_train_samples:]
+    def __init__(self, data: list):
         
         self.labels = label_encoder(
             [img_path.rsplit("/",1)[1].split("#",1)[0] for img_path in data]
         )
         self.data = data
-        self.data_dir = data_dir
 
     def __getitem__(self, idx): 
         x = nib.load(self.data[idx]).get_fdata()
@@ -62,4 +73,9 @@ class NiiDataset(Dataset):
     def __len__(self):
         # return the size of the dataset
         return len(self.data)
-    
+
+def kfold(dataset, n_splits=5):
+    idxs = itertools.cycle(KFold(n_splits).split(np.arange(len(dataset))))
+    for train_idxs, val_idxs in idxs:
+        yield torch.utils.data.Subset(dataset, train_idxs), torch.utils.data.Subset(dataset, val_idxs)
+        
