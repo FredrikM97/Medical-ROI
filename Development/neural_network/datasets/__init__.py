b682c3137f4b6b4d57a3d68ef038fd86b5575f3d
Fredrik <fredrik9779@gmail.com>
2021-02-09 18:46:09 +0100

    Removed deprecated code and changed module import

--- Development/neural_network/datasets/__init__.py
+++ Development/neural_network/datasets/__init__.py
@@ -3,7 +3,7 @@
 """
 import importlib
 from torch.utils import data
-from pytorch_lightning import LightningModule
+from pytorch_lightning import LightningDataModule
 
 def find_dataset_using_name(dataset_name):
     """Import the module "data/[dataset_name]_dataset.py".
@@ -18,7 +18,7 @@ def find_dataset_using_name(dataset_name):
     target_dataset_name = dataset_name.replace('_', '') + 'dataset'
     for name, cls in datasetlib.__dict__.items():
         if name.lower() == target_dataset_name.lower() \
-           and issubclass(cls, LightningModule):
+           and issubclass(cls, LightningDataModule):
             dataset = cls
 
     if dataset is None:
@@ -26,7 +26,7 @@ def find_dataset_using_name(dataset_name):
 
     return dataset
 
-def create_dataset(configuration):
+def create_dataset(configuration:dict):
     """Create a dataset given the configuration (loaded from the json file).
     This function wraps the class CustomDatasetDataLoader.
         This is the main interface between this package and train.py/validate.py
@@ -34,54 +34,5 @@ def create_dataset(configuration):
         from datasets import create_dataset
         dataset = create_dataset(configuration)
     """
-    #data_loader = CustomDatasetDataLoader(configuration)
-    #dataset = data_loader.load_data()
-    return find_dataset_using_name(configuration['dataset_name'])(configuration)
+    return find_dataset_using_name(configuration['dataset_name'])(**configuration)
 
-
-class CustomDatasetDataLoader():
-    """Wrapper class of Dataset class that performs multi-threaded data loading
-        according to the configuration.
-    """
-    def __init__(self, configuration):
-        self.configuration = configuration
-        dataset_class = find_dataset_using_name(configuration['dataset_name'])
-        self.dataset = dataset_class(configuration)
-        print("dataset [{0}] was created".format(type(self.dataset).__name__))
-
-        # if we use custom collation, define it as a staticmethod in the dataset class
-        custom_collate_fn = getattr(self.dataset, "collate_fn", None)
-        if callable(custom_collate_fn):
-            self.dataloader = data.DataLoader(self.dataset, **configuration['loader_params'], collate_fn=custom_collate_fn)
-        else:
-            self.dataloader = data.DataLoader(self.dataset, **configuration['loader_params'])
-
-
-    def load_data(self):
-        return self
-
-
-    def get_custom_dataloader(self, custom_configuration):
-        """Get a custom dataloader (e.g. for exporting the model).
-            This dataloader may use different configurations than the
-            default train_dataloader and val_dataloader.
-        """
-        custom_collate_fn = getattr(self.dataset, "collate_fn", None)
-        if callable(custom_collate_fn):
-            custom_dataloader = data.DataLoader(self.dataset, **self.configuration['loader_params'], collate_fn=custom_collate_fn)
-        else:
-            custom_dataloader = data.DataLoader(self.dataset, **self.configuration['loader_params'])
-        return custom_dataloader
-    
-
-    def __len__(self):
-        """Return the number of data in the dataset.
-        """
-        return len(self.dataset)
-
-
-    def __iter__(self):
-        """Return a batch of data.
-        """
-        for data in self.dataloader:
-            yield data
