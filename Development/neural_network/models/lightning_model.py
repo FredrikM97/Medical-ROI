1d53544bf81429b76cad8eb6217460bb88075971
Fredrik <fredrik9779@gmail.com>
2021-02-11 20:53:27 +0100

    Added confusion matrix, refactor and removed unused code.

--- Development/neural_network/models/lightning_model.py
+++ Development/neural_network/models/lightning_model.py
@@ -5,47 +5,45 @@ import torch.nn as nn
 import torch
 from pytorch_lightning.metrics import functional as FM
 from pytorch_lightning.metrics import Accuracy
-
+from pytorch_lightning.metrics.classification import ConfusionMatrix
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
 # https://pytorch-lightning.readthedocs.io/en/latest/metrics.html
 class LightningModel(pl.LightningModule): 
     def __init__(self, **hparams):
         super().__init__() 
-        self.save_hyperparameters()
         
-        self.model = create_architecture(architecture=self.hparams.architecture,input_channels=1, num_classes=3)#testModel(input_channels=1, num_classes=3)
-        self.loss = nn.CrossEntropyLoss() 
-        self.lr = self.hparams.lr
+        
+        self.model = create_architecture(architecture=hparams['architecture'],input_channels=1, num_classes=3)#testModel(input_channels=1, num_classes=3)
+        
+        self.save_hyperparameters()
         
         # These have internal memory
         self.train_metric = Accuracy(compute_on_step=False)
         self.val_metric = Accuracy(compute_on_step=False)
+        self.train_cm = ConfusionMatrix(3, compute_on_step=False)
         
-        
-        
-        self.save_hyperparameters()
+    def forward(self, x):
+        return self.model(x)
         
     def training_step(self, batch: dict, batch_idx: int) -> dict:
         x, target = batch
-        pred = self.model(x) 
-        loss = self.loss(pred, target) 
-        print("target",target,"predict",pred)
-        self.train_metric(pred,target)
-        self.logger.experiment.add_scalars(f"prediction_vs_target/train",{'predict':pred, 'target':target})
+        pred = self.forward(x)
+  
+        loss = self.loss_fn(pred, target) 
         
-        return {'loss':loss, 'predict':pred, 'target':target}
+        self.train_metric(pred,target)
+        self.train_cm(pred,target)
+        return {'loss':loss}
   
     def validation_step(self, batch: dict, batch_idx: int) -> dict:
         x, target = batch
-        pred = self.model(x) 
-        loss = self.loss(pred, target) 
+        pred = self.forward(x)
+        loss = self.loss_fn(pred, target) 
         
         self.val_metric(pred,target)
-        
-        return {'loss':loss, 'predict':pred, 'target':target}
-    
-    def on_epoch_end(self):
-        self.trainer.datamodule._on_epoch_end()
-       
+        return {'loss':loss}
     
     def training_epoch_end(self, outputs):
         # logging histograms
@@ -53,26 +51,29 @@ class LightningModel(pl.LightningModule):
         
         loss = avg_metric(outputs, 'loss')
         acc = self.train_metric.compute()
-        
-        self._add_scalar('train', loss, acc)
+        self._log_cm()
+        self._log_scalar('train', loss, acc)
     
     def validation_epoch_end(self, outputs):
         loss = avg_metric(outputs, 'loss')
         acc = self.val_metric.compute()
         
-        self._add_scalar('val', loss, acc)
+        self._log_scalar('val', loss, acc)
         
-    
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return [optimizer]
+        # Note: dont use list if only one item.. Causes silent crashes
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.lr)
+        return optimizer
     
+    def loss_fn(self,out,target):
+        return nn.CrossEntropyLoss()(out,target)
+
     def custom_histogram_adder(self):
         # iterating through all parameters
         for name,params in self.model.named_parameters():
             self.logger.experiment.add_histogram(name,params,self.current_epoch)
     
-    def _add_scalar(self, prefix, loss, acc):
+    def _log_scalar(self, prefix, loss, acc):
         self.logger.experiment.add_scalar(f"loss/"+prefix,
                                             loss,
                                             self.current_epoch)
@@ -80,6 +81,17 @@ class LightningModel(pl.LightningModule):
         self.logger.experiment.add_scalar(f"accuracy/"+prefix,
                                             acc,
                                             self.current_epoch)
+    def _log_cm(self):
+        fig=plt.figure();
+        #plt.ylabel('True label')
+        #plt.xlabel('Predicted label')
+        cm = self.train_cm.compute()
+        ax = sns.heatmap(cm.cpu(), annot=True, annot_kws={"size": 12})
+        ax.set_xlabel("Predicted label")
+        ax.set_ylabel("True label")
+        self.logger.experiment.add_figure("confmat/train", fig,self.current_epoch)
+        #print("show confusion matrix",cm)
+        
 def avg_metric(outputs, metric):
     return torch.stack([x[metric] for x in outputs]).mean()
 
