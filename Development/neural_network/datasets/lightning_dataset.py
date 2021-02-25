2e7d6d64ffdf1743a23fbde084ed4acc2c03fdec
Fredrik <fredrik9779@gmail.com>
2021-02-25 18:15:59 +0100

    Split up classes and fix major bug where 50% of dataset was allocated for validation

--- Development/neural_network/datasets/lightning_dataset.py
+++ Development/neural_network/datasets/lightning_dataset.py
@@ -10,65 +10,61 @@ from sklearn.model_selection import KFold
 import itertools
 import numpy as np
 import sklearn
+from sklearn.model_selection import train_test_split
 
 class LightningDataset(pl.LightningDataModule): 
-    
-    def __init__(self,**hparams:dict):
+    trainset,testset,valset = (0,0,0)
+    def __init__(self,dataset_path=None,enable_testset=False,input_shuffle=True, input_seed=0, test_size=0.1,train_params={},val_params={},**hparams:dict):
         super().__init__()
         self._hparams = hparams
-        self.img_files = get_nii_files(self._hparams['dataset_path'])
+        self.dataset_path = dataset_path
+        self.train_params = train_params
+        self.val_params = val_params
+        self.enable_testset=enable_testset
+        self.input_shuffle = input_shuffle
+        self.img_files = get_nii_files(dataset_path)
+        self.input_seed = input_seed
+        self.test_size = test_size
         
-        assert len(self.img_files) > 0, f"No images found at path: {self._hparams['dataset_path']}"
-        self._setup_kfold()
+        assert len(self.img_files) > 0, f"No images found at path: {dataset_path}"
+        self._setup()
     
-    def _setup_kfold(self): 
-        dataset = NiiDataset(self.img_files)
+    def _setup(self): 
+        #dataset = NiiDataset(self.img_files)
         
-        self.get_current_fold = 0
-        self.kfold_splits = kfold(dataset, n_splits=self._hparams['fold_splits'], shuffle=True)
-        self._next_fold()
-        
-    def _get_subset(self):
-        return self.train_set, self.val_set
-    
-    def _get_fold(self) -> int:
-        return self.get_current_fold
-    
-    def _has_folds(self) -> bool:
-        return self.get_current_fold < self._hparams['fold_splits']
-        
-    def _next_fold(self) -> None:
-        assert self._has_folds(), "Cant find more folds!"
-        self.get_current_fold += 1
-        self.train_set, self.val_set = next(self.kfold_splits)
-        
-    def _get_class_weights(self):
-        labels = label_encoder(
-            [img_path.rsplit("/",1)[1].split("#",1)[0] for img_path in self.img_files]
-        )
-        
-        unique_classes = np.unique(labels)
+        if self.input_shuffle:
+            self.img_files = sklearn.utils.shuffle(self.img_files, random_state=self.seed)
+            
+        if self.enable_testset: 
+            self.trainset, self.testset = split_data(self.img_files, test_size=self.test_size)
+        else:
+            self.trainset = self.img_files
+            
+            
+        #self.get_current_fold = 0
+        #self.kfold_splits = kfold(dataset, n_splits=self._hparams['fold_splits'], shuffle=True)
+        #self._next_fold()
+        self.trainset, self.valset = split_data(self.trainset)
 
-        # Compute weights
-        return torch.from_numpy(sklearn.utils.class_weight.compute_class_weight('balanced', classes=unique_classes, y=labels.numpy())).float().cuda()
+        print(f"Dataset sizes - Training: {len(self.trainset)} Validation: {len(self.valset)} Test: {len(self.testset)}")
     
     def train_dataloader(self):
         #assert len(self.train_set) % self._hparams['train_params']['batch_size'] != 1, "A batch of size 1 is not allowed!"
 
-        dataloader = DataLoader(self.train_set,
-                                batch_size=self._hparams['train_params']['batch_size'],
+        dataloader = DataLoader(self.trainset,
+                                batch_size=self.train_params['batch_size'],
                                 shuffle=True,
-                                num_workers=self._hparams['train_params']['num_workers'])
+                                num_workers=self.train_params['num_workers'])
         return dataloader
     
     def val_dataloader(self):
         #assert len(self.val_set) % self._hparams['train_params']['batch_size'] != 1, "A batch of size 1 is not allowed!"
         #dataset = NiiDataset(train=False, data_dir=self.img_files)
         
-        dataloader = DataLoader(self.val_set,
-                                batch_size=self._hparams['val_params']['batch_size'],
+        dataloader = DataLoader(self.valset,
+                                batch_size=self.val_params['batch_size'],
                                 shuffle=False,
-                                num_workers=self._hparams['val_params']['num_workers'])
+                                num_workers=self.val_params['num_workers'])
 
         return dataloader
     
@@ -91,8 +87,64 @@ class NiiDataset(Dataset):
         # return the size of the dataset
         return len(self.data)
 
-def kfold(dataset, n_splits=5, shuffle=False):
-    idxs = KFold(n_splits, shuffle=shuffle).split(np.arange(len(dataset)))
-    for train_idxs, val_idxs in idxs:
-        yield torch.utils.data.Subset(dataset, train_idxs), torch.utils.data.Subset(dataset, val_idxs)
+class ClassWeights:
+    unique_classes = None
+    labels = None
+    weights = None
+    delimiter = '#'
+    def __init__(self, classes:dict):
+        self.unique_classes=classes
         
+    def compute(self, images):
+        self.labels = np.numpy([self.classes[img_path.rsplit("/",1)[1].split(self.delimiter,1)[0]] for img_path in data]) # Modified for this dataset..
+        
+        # Compute weights
+        self.weights = sklearn.utils.class_weight.compute_class_weight('balanced', classes=list(self.unique_classes.keys()), y=labels)
+    
+    def get_tensor(self):
+        return torch.from_numpy(self.weights).float()#.cuda()
+    
+    def get(self):
+        return self.weights
+        
+class Kfold:
+    
+    def __init__(self, dataset, n_splits=5, shuffle=False, random_state=None):
+        self._trainData = None
+        self._valData = None
+        self._fold_idx = None
+        self._n_splits = None
+        self.shuffle=None
+        self.random_state = random_state
+        self.kfold(dataset, n_splits, shuffle=shuffle, random_state=random_state)
+        
+    def kfold(self,dataset, n_splits, shuffle=False, random_state=None):
+        self.shuffle=shuffle
+        self._n_splits = n_splits
+        idxs = KFold(n_splits, shuffle=shuffle, random_state=random_state).split(np.arange(len(dataset)))
+        self._set(((torch.utils.data.Subset(dataset, train_idxs), torch.utils.data.Subset(dataset, val_idxs)) for train_idxs, val_idxs in idxs))
+        self.next()
+    
+    def _set(self,folds):
+        self._fold_idx = 1
+        self._folds = folds
+        
+    def get(self):
+        return self._trainData, self._valData
+    
+    def index(self) -> int:
+        return self._fold_idx
+    
+    def size(self):
+        return self._n_splits
+    
+    def exists(self) -> bool:
+        return self.index() < self.size()
+        
+    def next(self) -> None:
+        assert self.exists(), "Cant find more folds!"
+        self._fold_idx += 1
+        self._trainData, self._valData = next(self._folds)
+    
+def split_data(data,test_size=0.1, random_state=None, shuffle=False):
+    return train_test_split(data, test_size=test_size, random_state=random_state, shuffle=shuffle)
